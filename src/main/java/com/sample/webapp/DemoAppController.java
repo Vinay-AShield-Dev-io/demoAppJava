fb1963188eb8cfeaf3fbe7ecd82ea28ddba53f51
Vinay-AShield-Dev-io <96040758+Vinay-AShield-Dev-io@users.noreply.github.com>
2023-12-20 17:37:43 +0530

    migrating from request parameter to request body

--- src/main/java/com/sample/webapp/DemoAppController.java
+++ src/main/java/com/sample/webapp/DemoAppController.java
@@ -3,68 +3,95 @@ package com.sample.webapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sample.CDRLogging.CDRLoggingThread;
 import com.sample.datapojo.Users;
 import com.sample.dbservices.DBService;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+class LoginRequestObjectBody {
+	String username;
+	String password;
+}
+
+@Setter
+@Getter
+class RegistrationRequestObjectBody {
+	String username;
+	String password;
+	String age;
+}
+
 @Controller
 public class DemoAppController {
 	@Autowired
 	DBService demoDBServices;
 
-	
 	@PostMapping("/login")
 	@ResponseBody
-	String loginUser(@RequestParam(value = "username", required = true) String username,
-			@RequestParam(value = "password", required = true) String pass) {
-		Users user = demoDBServices.getByUsername(username);
-		if (user != null) {
-			String passHash = user.getHashPass();
-			if (passHash.equals(pass)) {
-				user.getAge();
-				CDRLoggingThread.write("Login request,"+ username +",AS201" );
-				return "Welcome back " + username;
+	String loginUser(@RequestBody LoginRequestObjectBody body) {
+		try {
+			String username = body.getUsername();
+			String pass = body.getPassword();
+			Users user = demoDBServices.getByUsername(username);
+			if (user != null) {
+				String passHash = user.getHashPass();
+				if (passHash.equals(pass)) {
+					user.getAge();
+					CDRLoggingThread.write("User Logged in");
+					return "Welcome back " + username;
+				} else {
+					CDRLoggingThread.write("Incorrect Username or Password");
+					return "Incorrect Username or Password";
+				}
 			} else {
-				CDRLoggingThread.write("Login request,"+ username +",AS1XX" );
-				return "Incorrect Username or Password";
+				CDRLoggingThread.write("User not found");
+				return "User not found";
 			}
-		} else {
-			CDRLoggingThread.write("Login request,"+ username +",AS1X2" );
-			return "User not found";
+		} catch (Exception exp) {
+			System.out.println(exp);
+			return "Sorry, Something went wrong!";
 		}
 	}
 
 	@PostMapping("/register")
 	@ResponseBody
-	String registerUser(@RequestParam(value = "username", required = true) String username,
-			@RequestParam(value = "password", required = true) String pass,
-			@RequestParam(value = "age", required = true) String age
-
-	) {
-		int parsedAge = 0;
+	String registerUser(@RequestBody RegistrationRequestObjectBody body) {
 		try {
-			parsedAge = Integer.parseInt(age);
-		} catch(NumberFormatException n) {
-			return "Invalid Age. Age should be a numerical value.";
+			String age = body.getAge();
+			String username = body.getUsername();
+			String pass = body.getPassword();
+			int parsedAge = 0;
+			try {
+				parsedAge = Integer.parseInt(age);
+			} catch (NumberFormatException n) {
+				return "Invalid Age. Age should be a numerical value.";
+			}
+			if (parsedAge == 0 || parsedAge > 200) {
+				return "Invalid age. age should be between 1 to 200";
+			}
+			Users user = demoDBServices.getByUsername(username);
+			if (user != null) {
+				return username + " already registered.";
+			}
+
+			Users u = new Users();
+			u.setUsername(username);
+			u.setHashPass(pass);
+			u.setAge(parsedAge);
+			demoDBServices.saveUserEntity(u);
+			CDRLoggingThread.write("Registration request for " + username + " was " + "successful");
+			return "token";
+		} catch (Exception exp) {
+			System.out.println(exp);
+			return "Sorry, Something went wrong!";
 		}
-		if(parsedAge == 0 || parsedAge > 200) {
-			return "Invalid age. age should be between 1 to 200";
-		}
-		Users user = demoDBServices.getByUsername(username);
-		if(user != null) {
-			return "User already registered.";
-		}
-		
-		Users u = new Users();
-		u.setUsername(username);
-		u.setHashPass(pass);
-		u.setAge(parsedAge);
-		demoDBServices.saveUserEntity(u);
-		CDRLoggingThread.write("Registration request," + username + "," + "AS201");
-		return "token";
 	}
 
 }
